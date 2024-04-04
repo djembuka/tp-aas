5cd5899302ae5f455b743121540dbe433ccc115d
djembuka <djembuka@gmail.com>
2024-04-04 16:24:36 +0300

    Check.detail.sign roles and types

--- source/components/check.detail.sign/storeFiles.js
+++ source/components/check.detail.sign/storeFiles.js
@@ -1,19 +1,38 @@
 window.checkDetailSignStore = {
   vkkrid: 55564,
+  signedParameters: 'some',
+  roles: [
+    {
+      code: 0,
+      value: 'Объект проверки',
+    },
+    {
+      code: 1,
+      value: 'Подписи эксперта',
+    },
+    {
+      code: 2,
+      value: 'Подписи куратора',
+    },
+  ],
   files: [
     {
       id: 6,
+      role: 0,
+      type: 1,
       fileid: 456,
-      filename: 'Постановление о проведении',
+      filename:
+        'Постановление о проведении проверки Ананьев Игорь Владимирович (11782631892674) №298374',
       filelink:
         '/upload/iblock/0f3/blo6k046lx0ov82p38dd9bjukyoygi65/Ustav-SRO-AAS-ot-28-10-22_-utverzhdennyi_-Minyustom-s-otmetkami-MYU.pdf',
       signed: false,
     },
     {
       id: 1,
+      role: 0,
+      type: 1,
       fileid: 456,
-      filename:
-        'Постановление о проведении проверки Ананьев Игорь Владимирович (11782631892674) №298374',
+      filename: 'Постановление о проведении проверки №298374',
       filelink:
         '/upload/iblock/0f3/blo6k046lx0ov82p38dd9bjukyoygi65/Ustav-SRO-AAS-ot-28-10-22_-utverzhdennyi_-Minyustom-s-otmetkami-MYU.pdf',
       status: {
@@ -27,12 +46,15 @@ window.checkDetailSignStore = {
     },
     {
       id: 7,
+      role: 1,
+      type: 1,
       fileid: 456,
       filename:
-        'Уведомление Планом внешних проверок деятельности работы аудиторских организаций (22006155925_7635.pdf)',
+        'Постановление о проведении проверки Ананьев Игорь Владимирович (11782631892674) №298374',
       filelink:
         '/upload/iblock/0f3/blo6k046lx0ov82p38dd9bjukyoygi65/Ustav-SRO-AAS-ot-28-10-22_-utverzhdennyi_-Minyustom-s-otmetkami-MYU.pdf',
       signed: true,
+      author: 'Подписан: <a href="?123">Азарянц Ашот Александрович</a>',
       date: 'Подписано 30.01.2024 16:59:60',
       status: {
         id: 1,
@@ -43,9 +65,31 @@ window.checkDetailSignStore = {
     },
     {
       id: 8,
+      role: 1,
+      type: 1,
+      fileid: 456,
+      filename: 'Постановление о проведении проверки №298374',
+      filelink:
+        '/upload/iblock/0f3/blo6k046lx0ov82p38dd9bjukyoygi65/Ustav-SRO-AAS-ot-28-10-22_-utverzhdennyi_-Minyustom-s-otmetkami-MYU.pdf',
+      signed: false,
+    },
+    {
+      id: 8,
+      role: 2,
+      type: 1,
       fileid: 456,
       filename:
-        'Обязательство о соблюдении конфиденциальности Ананьев Игорь Владимирович (11782631892674) №298374',
+        'Постановление о проведении проверки Ананьев Игорь Владимирович (11782631892674) №298374',
+      filelink:
+        '/upload/iblock/0f3/blo6k046lx0ov82p38dd9bjukyoygi65/Ustav-SRO-AAS-ot-28-10-22_-utverzhdennyi_-Minyustom-s-otmetkami-MYU.pdf',
+      signed: false,
+    },
+    {
+      id: 8,
+      role: 2,
+      type: 1,
+      fileid: 456,
+      filename: 'Постановление о проведении проверки №298374',
       filelink:
         '/upload/iblock/0f3/blo6k046lx0ov82p38dd9bjukyoygi65/Ustav-SRO-AAS-ot-28-10-22_-utverzhdennyi_-Minyustom-s-otmetkami-MYU.pdf',
       signed: false,
